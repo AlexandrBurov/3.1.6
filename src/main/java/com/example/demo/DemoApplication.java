e57a599def81c969316be9268e0f1515abbf6fa7
macbook <a.d.burov@mail.ru>
2022-11-17 10:04:49 +0400

    .

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,73 +1,59 @@
 package com.example.demo;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 
 
-//@SpringBootApplication
 public class DemoApplication {
 
 	private static final String url = "http://94.198.50.185:7081/api/users";
 	private static final String url3 = "http://94.198.50.185:7081/api/users/3";
 	static RestTemplate restTemplate = new RestTemplate();
-
-	static String cookie ;
-
+	static String cookie;
 
 	public static void main(String[] args) {
 
-
-//		SpringApplication.run(DemoApplication.class, args);
-
-	get();
-	post();
-  put();
+		get();
+		post();
+		put();
 		delete();
-
-
 
 	}
 
 
-private static void get(){
-// создае headers
-	HttpHeaders headers = new HttpHeaders();
-	headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-// создае сущность
-	HttpEntity<Object> requestEntity = new HttpEntity<>(headers);
+	private static void get() {
+// создаем headers
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+// создаем сущность
+		HttpEntity<Object> requestEntity = new HttpEntity<>(headers);
 // запрос
-	ResponseEntity<String> result = restTemplate.exchange(url, HttpMethod.GET, requestEntity, String.class);
+		ResponseEntity<String> result = restTemplate.exchange(url, HttpMethod.GET, requestEntity, String.class);
 
 // ответ
-	HttpStatus statusCode = result.getStatusCode();
-	System.out.println("status code - " + statusCode);
+		HttpStatus statusCode = result.getStatusCode();
+		System.out.println("status code - " + statusCode);
 
-	String user = result.getBody();//помощью getBody(). Это и будет тот самый 6-ти значны
-	System.out.println("response body - " + user);
+		String user = result.getBody();//помощью getBody(). Это и будет тот самый 6-ти значны
+		System.out.println("response body - " + user);
 
-	HttpHeaders responseHeaders = result.getHeaders();
-	System.out.println("response headers - " + responseHeaders.getFirst("Set-Cookie"));
-	 cookie = responseHeaders.getFirst("Set-Cookie");
+		HttpHeaders responseHeaders = result.getHeaders();
+		System.out.println("response headers - " + responseHeaders.getFirst("Set-Cookie"));
+		cookie = responseHeaders.getFirst("Set-Cookie");
 
-}
+	}
 
 
 	private static void post() {
 
-		User user = new User(3,"James","Brown",32);
+		User user = new User(3, "James", "Brown", 32);
 
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		headers.add("Cookie", cookie);
-		HttpEntity<User> entity = new HttpEntity<User>(user,headers);
+		HttpEntity<User> entity = new HttpEntity<User>(user, headers);
 
 		ResponseEntity<String> result = restTemplate.exchange(
 				url, HttpMethod.POST, entity, String.class);
@@ -75,7 +61,7 @@ private static void get(){
 		HttpStatus statusCode = result.getStatusCode();
 		System.out.println("status code - " + statusCode);
 
-		String body = result.getBody();//помощью getBody(). Это и будет тот самый 6-ти значны
+		String body = result.getBody();
 		System.out.println("response body - " + body);
 
 		HttpHeaders responseHeaders = result.getHeaders();
@@ -89,7 +75,7 @@ private static void get(){
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		headers.add("Cookie", cookie);
 
-		HttpEntity<User> entity = new HttpEntity<User>(new User(3,"Thomas","Shelby",32),headers);
+		HttpEntity<User> entity = new HttpEntity<User>(new User(3, "Thomas", "Shelby", 32), headers);
 
 		ResponseEntity<String> result = restTemplate.exchange(
 				url, HttpMethod.PUT, entity, String.class);
@@ -97,7 +83,7 @@ private static void get(){
 		HttpStatus statusCode = result.getStatusCode();
 		System.out.println("status code - " + statusCode);
 
-		String body = result.getBody();//помощью getBody(). Это и будет тот самый 6-ти значны
+		String body = result.getBody();
 		System.out.println("response body - " + body);
 
 		HttpHeaders responseHeaders = result.getHeaders();
@@ -119,16 +105,13 @@ private static void get(){
 		HttpStatus statusCode = result.getStatusCode();
 		System.out.println("status code - " + statusCode);
 
-		String body = result.getBody();//помощью getBody(). Это и будет тот самый 6-ти значны
+		String body = result.getBody();
 		System.out.println("response body - " + body);
 
 		HttpHeaders responseHeaders = result.getHeaders();
 		System.out.println("response headers - " + responseHeaders);
 
 	}
-
-
-
 
 }
 
